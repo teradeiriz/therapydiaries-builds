96d9a0cbbbdb7cd2ebb6f6479d871ac88a0da123
teradeiriz <therapydiaries.app@gmail.com>
2020-05-30 22:42:15 +0300

    Cleaned code

--- scripts/ios-prod-check.sh
+++ scripts/ios-prod-check.sh
@@ -40,25 +40,11 @@ then
     exit 1
 fi
 
-# TODO check for android
-# if [[ -z "$ENCRYPT_PASSWORD" ]]
-# then
-#     echo "Please sure that ENCRYPT_PASSWORD exists"
-#     exit 1
-# fi
-
 if [[ -z "$DATABASE_DEFAULT_ENCRYPTION_KEY" ]]
 then
     echo "Please sure that DATABASE_DEFAULT_ENCRYPTION_KEY exists"
     exit 1
 fi
-
-# TODO check for android
-# if [[ -z "$OAUTH_CLIENT_ID_ANDROID" ]]
-# then
-#     echo "Please sure that OAUTH_CLIENT_ID_ANDROID exists"
-#     exit 1
-# fi
 
 if [[ -z "$APP_METRICA_API_KEY" ]]
 then
