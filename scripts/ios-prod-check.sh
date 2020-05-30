89cb3b0f58052bb6f7a02ba6b53221a8f158abca
natallia.chobat <natallia.chobat@gmail.com>
2020-05-30 22:44:43 +0300

    Deleted unused check

--- scripts/ios-prod-check.sh
+++ scripts/ios-prod-check.sh
@@ -52,12 +52,6 @@ then
     exit 1
 fi
 
-if [[ -z "$FIREBASEAPPDISTRO_APP" ]]
-then
-    echo "Please sure that FIREBASEAPPDISTRO_APP exists"
-    exit 1
-fi
-
 if [[ -z "$FIREBASE_TOKEN" ]]
 then
     echo "Please sure that FIREBASE_TOKEN exists"
