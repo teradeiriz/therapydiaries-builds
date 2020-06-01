cb572d25e49b4bb90de717261d1a73c748b6214d
natallia.chobat <natallia.chobat@gmail.com>
2020-06-01 09:58:59 +0300

    Improved checks

--- scripts/ios-prod-check.sh
+++ scripts/ios-prod-check.sh
@@ -4,54 +4,6 @@ set -e
 # print all env variables
 # printenv | cat
 
-if [[ -z "$USER_NAME" ]]
-then
-    echo "Please sure that USER_NAME exists"
-    exit 1
-fi
-
-if [[ -z "$USER_EMAIL" ]]
-then
-    echo "Please sure that USER_EMAIL exists"
-    exit 1
-fi
-
-if [[ -z "$REPO_URL" ]]
-then
-    echo "Please sure that REPO_URL exists"
-    exit 1
-fi
-
-if [[ -z "$SSH_KNOWN_HOST" ]]
-then
-    echo "Please sure that SSH_KNOWN_HOST exists"
-    exit 1
-fi
-
-if [[ -z "$SSH_PRIVATE_KEY" ]]
-then
-    echo "Please sure that SSH_PRIVATE_KEY exists"
-    exit 1
-fi
-
-if [[ -z "$CURRENT_BRANCH" ]]
-then
-    echo "Please sure that CURRENT_BRANCH exists"
-    exit 1
-fi
-
-if [[ -z "$DATABASE_DEFAULT_ENCRYPTION_KEY" ]]
-then
-    echo "Please sure that DATABASE_DEFAULT_ENCRYPTION_KEY exists"
-    exit 1
-fi
-
-if [[ -z "$APP_METRICA_API_KEY" ]]
-then
-    echo "Please sure that APP_METRICA_API_KEY exists"
-    exit 1
-fi
-
 if [[ -z "$FASTLANE_USER" ]]
 then
     echo "Please sure that FASTLANE_USER exists"
@@ -88,8 +40,8 @@ then
     exit 1
 fi
 
-if [[ -z "$OAUTH_CLIENT_ID_ANDROID" ]]
+if [[ -z "$BUMP_TYPE" ]]
 then
-    echo "Please sure that OAUTH_CLIENT_ID_ANDROID exists"
+    echo "Please sure that BUMP_TYPE exists"
     exit 1
 fi
